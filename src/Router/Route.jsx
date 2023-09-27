166c842c905984fe604312c74364ac185a66f644
Mostaq27 <mostaq99muzahid@gmail.com>
2023-09-27 18:49:58 +0600

    starting project and create react router path

---
+++ src/Router/Route.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+const  myCreatedRoute = createBrowserRouter([
+    {
+
+
+
+        path : "/",
+        element :<div>hello react</div>,
+    }
+       
+])
+
+export default myCreatedRoute;
